bb7dbb73e794bde031a389cc286423244566c0b2
SalamAwal <ssa5693@psu.edu>
2026-09-23 13:59:16 -0400

    Update script_demo_2026Fall.m

--- script_demo_2026Fall.m
+++ script_demo_2026Fall.m
@@ -1,4 +1,4 @@
-%% Introduction to and Purpose of the Code
+ %% Introduction to and Purpose of the Code
 % This is the explanation of the code that can be found by running
 %
 %       script_demo_2026Spring.m
